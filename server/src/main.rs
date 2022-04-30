8c0eb84d4e45923c74291c4f336e4a369ac6c1ba
WillRazorFace <willrazorface90s@gmail.com>
2022-04-30 15:35:01 -0300

    Server-side

---
+++ server/src/main.rs
@@ -0,0 +1,48 @@
+use std::io;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::str::from_utf8;
+
+const PORT: &str = "2202";
+
+fn send_data(mut stream: &TcpStream, data: String) {
+    stream.write_all(data.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn receive_data(mut stream: &TcpStream) -> io::Result<String> {
+    let mut buffer = [0u8; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    stream.flush().unwrap();
+
+    Ok(String::from(from_utf8(&buffer).unwrap()))
+}
+
+fn main() {
+    let server: TcpListener = TcpListener::bind(format!("0.0.0.0:{}", PORT)).unwrap();
+
+    for client in server.incoming() {
+        let client = client.unwrap();
+        let addr = client.local_addr().unwrap();
+
+        println!("Connectd with ({}). Enjoy!\n", addr);
+
+        loop {
+            print!("(You) > ");
+            io::stdout().flush().unwrap();
+
+            let mut data = String::new();
+
+            io::stdin().read_line(&mut data).unwrap();
+            io::stdout().flush().unwrap();
+
+            send_data(&client, data);
+
+            let response: String = receive_data(&client).unwrap();
+
+            print!("({}) > {}", addr, response);
+            io::stdout().flush().unwrap();
+        }
+    }
+}
