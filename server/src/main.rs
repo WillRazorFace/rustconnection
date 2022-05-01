175c45539be1e981a62674197475856cd52fb508
WillRazorFace <willrazorface90s@gmail.com>
2022-05-01 10:27:58 -0300

    Async echo server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,53 +1,25 @@
-use std::io;
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::str::from_utf8;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
 
 const PORT: &str = "2202";
 
-fn send_data(mut stream: &TcpStream, data: String) {
-    stream.write_all(data.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
+#[tokio::main]
+async fn main() {
+    let server = TcpListener::bind(format!("0.0.0.0:{}", PORT))
+        .await
+        .unwrap();
 
-fn receive_data(mut stream: &TcpStream) -> io::Result<String> {
-    let mut buffer = [0u8; 1024];
+    let (mut client, addr) = server.accept().await.unwrap();
 
-    stream.read(&mut buffer).unwrap();
-    stream.flush().unwrap();
-
-    Ok(String::from(from_utf8(&buffer).unwrap()))
-}
-
-fn handler(stream: TcpStream) {
-    let addr = stream.local_addr().unwrap();
-
-    println!("Connectd with ({}). Enjoy!\n", addr);
+    println!("Connection from ({})!", addr);
 
     loop {
-        print!("(You) > ");
-        io::stdout().flush().unwrap();
+        let mut buffer = [0u8; 1024];
 
-        let mut data = String::new();
+        let bytes_read = client.read(&mut buffer).await.unwrap();
 
-        io::stdin().read_line(&mut data).unwrap();
-        io::stdout().flush().unwrap();
-
-        send_data(&stream, data);
-
-        let response: String = receive_data(&stream).unwrap();
-
-        print!("({}) > {}", addr, response);
-        io::stdout().flush().unwrap();
-    }
-}
-
-fn main() {
-    let server: TcpListener = TcpListener::bind(format!("0.0.0.0:{}", PORT)).unwrap();
-
-    for client in server.incoming() {
-        let client = client.unwrap();
-
-        handler(client);
+        client.write_all(&buffer[..bytes_read]).await.unwrap();
     }
 }
