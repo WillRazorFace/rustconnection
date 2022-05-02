2c77f0499b832a9709922684a63be18f54c58da2
WillRazorFace <willrazorface90s@gmail.com>
2022-05-02 16:44:15 -0300

    Clients list through "sessions" command

--- server/src/gen.rs
+++ server/src/gen.rs
@@ -1,5 +1,4 @@
-use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::{net::TcpStream, sync::Mutex};
 
-pub type Clients = Arc<Mutex<HashMap<String, TcpStream>>>;
+pub type Clients = Arc<Mutex<Vec<TcpStream>>>;
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,17 @@
 mod gen;
 use std::collections::HashMap;
+use std::io;
+use std::io::{Read, Write};
+use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
-use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
-    sync::Mutex,
-};
+use tokio::{net::TcpListener, sync::Mutex};
 
 const PORT: &str = "2202";
 
 async fn handle_clients(listener: TcpListener, clients_list: gen::Clients) {
     loop {
         let (client, addr) = listener.accept().await.unwrap();
-        clients_list.lock().await.insert(addr.to_string(), client);
-
-        println!("[+] Connection from ({}) [+]", addr);
+        clients_list.lock().await.push(client);
     }
 }
 
@@ -23,9 +20,44 @@ async fn main() {
     let server = TcpListener::bind(format!("0.0.0.0:{}", PORT))
         .await
         .unwrap();
-    let clients: gen::Clients = Arc::new(Mutex::new(HashMap::new()));
+    let clients: gen::Clients = Arc::new(Mutex::new(Vec::new()));
 
     tokio::spawn(handle_clients(server, clients.clone()));
 
-    loop {}
+    println!("\nType 'sessions' to see all clients");
+    print!(">>> ");
+    io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+
+    io::stdin().read_line(&mut command).unwrap();
+    println!("");
+
+    match command.as_str().trim() {
+        "sessions" => {
+            for (index, client) in clients.lock().await.deref().iter().enumerate() {
+                println!("[{}] {}", index, client.peer_addr().unwrap());
+            }
+
+            println!("\nSelect session");
+            print!(">>> ");
+            io::stdout().flush().unwrap();
+
+            let mut session = String::new();
+
+            io::stdin().read_line(&mut session).unwrap();
+            println!("");
+
+            let session: usize = match session.trim().parse() {
+                Ok(session) => session,
+                Err(e) => panic!("Conversion error: {}", e),
+            };
+
+            println!(
+                "\n{}",
+                clients.lock().await.get(session).unwrap().peer_addr().unwrap()
+            );
+        }
+        _ => {}
+    }
 }
