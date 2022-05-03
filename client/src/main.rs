97bd804ba5aa9e307cafeef33e73a4d55941e6a2
WillRazorFace <willrazorface90s@gmail.com>
2022-05-03 19:21:43 -0300

    Expendable functions removed and download/upload_file testing

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,43 +1,35 @@
 use std::io;
 use std::io::{Read, Write};
-use std::net::TcpStream;
 use std::str::from_utf8;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpStream,
+};
+use util;
 
 const HOST: &str = "localhost";
 const PORT: &str = "2202";
 
-fn receive_data(mut stream: &TcpStream) -> io::Result<String> {
-    let mut buffer = [0u8; 1024];
-
-    let bytes_read = stream.read(&mut buffer).unwrap();
-    stream.flush().unwrap();
-
-    Ok(String::from(from_utf8(&buffer[..bytes_read]).unwrap()))
-}
-
-fn send_data(mut stream: &TcpStream, data: String) {
-    stream.write_all(data.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let addr = format!("{}:{}", HOST, PORT);
 
-    match TcpStream::connect(&addr) {
-        Ok(stream) => {
+    match TcpStream::connect(&addr).await {
+        Ok(mut stream) => {
             println!("Connected in ({})\n", addr);
-            loop {
-                let response: String = receive_data(&stream).unwrap();
 
-                if response == "CHECK_ALIVE" {
-                    send_data(&stream, String::from(""));
-                }
+            let mut buffer = [0u8; 11];
 
-                loop {}
+            stream.read(&mut buffer).await.unwrap();
+
+            if from_utf8(&buffer).unwrap() == "CHECK_ALIVE" {
+                stream.write_all("ALIVE".as_bytes()).await.unwrap();
             }
+
+            util::download_file("test", stream).await.unwrap();
         }
         Err(e) => {
-            println!("Bad connection: {}", e);
+            println!("Bad connection: {}", e)
         }
     }
 }
